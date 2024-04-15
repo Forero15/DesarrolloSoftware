bb36c01692a7fa77b9869a4c9c3a03a29cb62b94
Forero15 <viviana15115@gmail.com>
2024-04-15 16:30:56 -0500

    cambio php

--- Cliente-Servidor/Login.php
+++ Cliente-Servidor/Login.php
@@ -1,17 +1,18 @@
 <html>
     <head>
-        <title>Schedule</title>
+        <meta charset="UTF-8">
+        <title>Login</title>
     </head>
     <body>
         <?php 
             $login = $_POST["txtLogin"];
             $pass = $_POST["txtPass"];
-            $suma = "$login - $pass";
+            
         ?>
-            <h1>HOLA LA SUMA ES: <?php echo $suma; ?> </h1>
+            
         
 
-        <h1>Schedule your next travel</h1>
+        <h1>Holi</h1>
         <form action="">
             Login <input type="text" name="txtLogin" value="<?php echo $login; ?>" />
             Contraseña: <input type="password" name="txtPass" value="<?php echo $pass; ?>" /> <br/>
